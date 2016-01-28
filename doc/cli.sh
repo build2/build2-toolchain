067acd6b49b94d4f1cba238d5530fbebeded1bf2
Boris Kolpackov <boris@codesynthesis.com>
2016-01-28 14:19:37 +0200

    Update intro styling

---
+++ doc/cli.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+
+version="0.2" # 0.2.0
+date="January 2016"
+
+trap 'exit 1' ERR
+set -o errtrace # Trap in functions.
+
+function info () { echo "$*" 1>&2; }
+function error () { info "$*"; exit 1; }
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    --clean)
+      rm -f build2-toolchain-intro.xhtml build2-toolchain-intro.ps \
+build2-toolchain-intro.pdf
+      exit 0
+      ;;
+    *)
+      error "unexpected $1"
+      ;;
+  esac
+done
+
+cli -I .. -v version="$version" -v date="$date" --generate-html \
+--html-prologue-file intro-prologue.xhtml \
+--html-epilogue-file intro-epilogue.xhtml \
+--html-suffix .xhtml --output-prefix build2-toolchain- intro.cli
+
+#html2ps -f intro.html2ps -o build2-toolchain-intro.ps build2-toolchain-intro.xhtml
+#ps2pdf14 build2-toolchain-intro.ps build2-toolchain-intro.pdf

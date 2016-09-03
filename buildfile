879d7e92d823c9dfe6fb3691541f30b662f2a510
Boris Kolpackov <boris@codesynthesis.com>
2016-09-03 17:27:33 +0200

    First take on new installation and upgrade instructions

--- buildfile
+++ buildfile
@@ -3,10 +3,22 @@
 # license   : MIT; see accompanying LICENSE file
 
 d = libbutl/ build2/ libsqlite3/ libodb/ libodb-sqlite/ libbpkg/ bpkg/ doc/
-./: $d doc{INSTALL README version} file{INSTALL.cli} \
+
+i =               \
+INSTALL           \
+UPGRADE           \
+BOOTSTRAP-MACOSX  \
+BOOTSTRAP-MINGW   \
+BOOTSTRAP-MSVC    \
+BOOTSTRAP-UNIX    \
+BOOTSTRAP-WINDOWS
+
+./: $d doc{$i README version} cli{$i} \
 file{build.sh build-msvc.bat build-mingw.bat}
+
 include $d
 
-# Don't install the INSTALL file.
+# Don't install the BOOTSTRAP/INSTALL files. But UPGRADE could be useful.
 #
 doc{INSTALL}@./: install = false
+doc{BOOTSTRAP-*}: install = false
--- doc/cli.sh
+++ doc/cli.sh
@@ -13,7 +13,9 @@ while [ $# -gt 0 ]; do
   case $1 in
     --clean)
       rm -f build2-toolchain-intro.xhtml build2-toolchain-intro*.ps \
-build2-toolchain-intro*.pdf
+ build2-toolchain-intro*.pdf
+      rm -f build2-toolchain-install.xhtml build2-toolchain-install*.ps \
+build2-toolchain-install*.pdf
       exit 0
       ;;
     *)
@@ -22,16 +24,42 @@ build2-toolchain-intro*.pdf
   esac
 done
 
-cli -I .. -v version="$version" -v date="$date" \
+function gen () # <name>
+{
+  local n="$1"
+  shift
+  cli -I .. -v version="$version" -v date="$date" \
 --generate-html --html-suffix .xhtml \
 --html-prologue-file doc-prologue.xhtml \
 --html-epilogue-file doc-epilogue.xhtml \
---link-regex '%b([-.].+)%../../build2/doc/b$1%' \
---link-regex '%bpkg([-.].+)%../../bpkg/doc/bpkg$1%' \
---output-prefix build2-toolchain- intro.cli
+--link-regex '%b([-.].+)%../../build2/doc/b$n%' \
+--link-regex '%bpkg([-.].+)%../../bpkg/doc/bpkg$n%' \
+--output-prefix build2-toolchain- "${@}" $n.cli
 
-html2ps -f doc.html2ps:a4.html2ps -o build2-toolchain-intro-a4.ps build2-toolchain-intro.xhtml
-ps2pdf14 -sPAPERSIZE=a4 -dOptimize=true -dEmbedAllFonts=true build2-toolchain-intro-a4.ps build2-toolchain-intro-a4.pdf
+html2ps -f doc.html2ps:a4.html2ps -o build2-toolchain-$n-a4.ps build2-toolchain-$n.xhtml
+ps2pdf14 -sPAPERSIZE=a4 -dOptimize=true -dEmbedAllFonts=true build2-toolchain-$n-a4.ps build2-toolchain-$n-a4.pdf
 
-html2ps -f doc.html2ps:letter.html2ps -o build2-toolchain-intro-letter.ps build2-toolchain-intro.xhtml
-ps2pdf14 -sPAPERSIZE=letter -dOptimize=true -dEmbedAllFonts=true build2-toolchain-intro-letter.ps build2-toolchain-intro-letter.pdf
+html2ps -f doc.html2ps:letter.html2ps -o build2-toolchain-$n-letter.ps build2-toolchain-$n.xhtml
+ps2pdf14 -sPAPERSIZE=letter -dOptimize=true -dEmbedAllFonts=true build2-toolchain-$n-letter.ps build2-toolchain-$n-letter.pdf
+}
+
+# Auto-heading doesn't work since it is broken into multiple doc strings.
+#
+gen install --html-heading-map 2=h2
+gen intro
+
+# Generate INSTALL/BOOTSTRAP/UPGRADE file in ../
+#
+function gen_txt () # <name>
+{
+  cli --generate-txt --omit-link-check --link-regex '%#(.*)%\1 file%' \
+-o .. --txt-suffix "" ../$1.cli
+}
+
+gen_txt INSTALL
+gen_txt UPGRADE
+gen_txt BOOTSTRAP-MACOSX
+gen_txt BOOTSTRAP-MINGW
+gen_txt BOOTSTRAP-MSVC
+gen_txt BOOTSTRAP-UNIX
+gen_txt BOOTSTRAP-WINDOWS

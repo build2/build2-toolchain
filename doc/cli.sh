2aa72e2e0e2551d986baeae7d57d628e58a61c39
Boris Kolpackov <boris@codesynthesis.com>
2016-09-04 17:19:27 +0200

    Update introduction

--- doc/cli.sh
+++ doc/cli.sh
@@ -32,8 +32,8 @@ function gen () # <name>
 --generate-html --html-suffix .xhtml \
 --html-prologue-file doc-prologue.xhtml \
 --html-epilogue-file doc-epilogue.xhtml \
---link-regex '%b([-.].+)%../../build2/doc/b$n%' \
---link-regex '%bpkg([-.].+)%../../bpkg/doc/bpkg$n%' \
+--link-regex '%b([-.].+)%../../build2/doc/b$1%' \
+--link-regex '%bpkg([-.].+)%../../bpkg/doc/bpkg$1%' \
 --output-prefix build2-toolchain- "${@}" $n.cli
 
 html2ps -f doc.html2ps:a4.html2ps -o build2-toolchain-$n-a4.ps build2-toolchain-$n.xhtml

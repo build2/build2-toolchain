c590daff1bfc968a99bc95a4cf4847b5e4b8fc02
Boris Kolpackov <boris@codesynthesis.com>
2017-05-05 13:00:48 +0200

    Bump version to 0.6.0-a.0.z, master is open for business

--- build-mingw.bat
+++ build-mingw.bat
@@ -36,14 +36,14 @@ if "_%1_" == "_/?_" goto usage
 rem Package repository URL (or path).
 rem
 if "_%BUILD2_REPO%_" == "__" (
-rem set "BUILD2_REPO=https://stage.build2.org/1"
+    set "BUILD2_REPO=https://stage.build2.org/1"
 rem set "BUILD2_REPO=https://pkg.cppget.org/1/queue"
-    set "BUILD2_REPO=https://pkg.cppget.org/1/alpha"
+rem set "BUILD2_REPO=https://pkg.cppget.org/1/alpha"
 )
 
 rem Bpkg configuration directory.
 rem
-set "cver=0.5"
+set "cver=0.6-a.0"
 set "cdir=build2-toolchain-%cver%"
 
 rem Compiler.

193a2968fedaea5afc0117a5653a30bcaf433270
Boris Kolpackov <boris@codesynthesis.com>
2018-05-30 10:09:21 +0200

    Switch default repository to public

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ usage="Usage: $0 [-h|--help] [<options>] <c++-compiler>"
 # Package repository URL (or path).
 #
 if test -z "$BUILD2_REPO"; then
-  BUILD2_REPO="https://stage.build2.org/1"
+# BUILD2_REPO="https://stage.build2.org/1"
 # BUILD2_REPO="https://pkg.cppget.org/1/queue"
-# BUILD2_REPO="https://pkg.cppget.org/1/alpha"
+  BUILD2_REPO="https://pkg.cppget.org/1/alpha"
 fi
 
 # The bpkg configuration directory.
